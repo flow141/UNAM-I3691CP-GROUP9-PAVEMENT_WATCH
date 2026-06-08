ab41f0a3bbf3e478f21c6c6f8a0ebca3d3a453d0
tiopolina  n nicodemus <tiopolinanicodemus7@gmail.com>
2026-06-08 22:11:17 +0200

    Create report_model.dart

---
+++ report_model.dart
@@ -0,0 +1,39 @@
+class Report {
+
+String imageUrl;
+
+double latitude;
+
+double longitude;
+
+String status;
+
+Report({
+
+required this.imageUrl,
+
+required this.latitude,
+
+required this.longitude,
+
+required this.status,
+
+});
+
+Map<String,dynamic> toMap(){
+
+return{
+
+'imageUrl':imageUrl,
+
+'latitude':latitude,
+
+'longitude':longitude,
+
+'status':status
+
+};
+
+}
+
+}
