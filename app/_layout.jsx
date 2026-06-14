e4976dea7104649373c03da6a02268c409601e03
Allex2004 <johannestuhafeni001@gmail.com>
2026-06-14 12:41:26 +0200

    Update _layout.jsx
    updated

--- app/_layout.jsx
+++ app/_layout.jsx
@@ -3,11 +3,15 @@ import { StatusBar } from 'expo-status-bar';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
 
 export default function RootLayout() {
+  const screenOptions = {
+    headerShown: false,
+    animation: 'slide_from_right',
+  };
+
   return (
     <SafeAreaProvider>
       <StatusBar style="dark" />
-      <Stack screenOptions={{ headerShown: false, animation: 'slide_from_right' }} />
+      <Stack screenOptions={screenOptions} />
     </SafeAreaProvider>
   );
 }
- 
